45edc3ca52b2b8e70d6610d5be697d91702f4e27
Taiga-stack <75029100+Taiga-stack@users.noreply.github.com>
2021-01-25 13:49:01 +0900

    Update Task1_4.java

--- 1-4/Task1_4.java
+++ 1-4/Task1_4.java
@@ -13,8 +13,8 @@ public class Task1_4 {
 
     // 定数（アカウント情報）
     // ① 登録されている名前とパスワードを定数で定義してください。
-    private static final String NAME = "alice";
-    private static final String PASSWORD = "pass";
+    String NAME = "alice";
+    String PASS = "pass";
 
     // 定数（メッセージ）
     private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
@@ -34,18 +34,17 @@ public class Task1_4 {
          String name = "alice";
          String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-        if(name == NAME && pass == PASSWORD){
-            System.out.println("ログイン成功です。");
+        if(name.equals("alice") && pass.equals("pass")){
+            System.out.println("CONST_MSG_SUCCESS);}
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        }else if(name == NAME){
-            System.out.println("パスワードに誤ります。");
+        else if(name.equals("alice ")){
+            System.out.println("CONST_MSG_ERROR_PASS");}
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        }else if(pass == PASSWORD){
-            System.out.println("名前に誤りがあります");
+        else if(pass.equals("pass")){
+            System.out.println("CONST_MSG_ERROR_NAME");}
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
-        }else{
-            System.out.println("入力情報に誤りがあります。");
+        else
+            System.out.println("CONST_MSG_ERROR_INPUT");}
         }
-    }
 
 }
